01bbdcfb441e9920c25596d642c004955aa21ade
Nacho Brito <nacho.brito@nutritienda.com>
2022-02-13 09:49:47 +0100

    perf: use weight symmetry in graph calculation

--- src/TextRank/domain/Graph.ts
+++ src/TextRank/domain/Graph.ts
@@ -16,18 +16,25 @@ export default class Graph {
      * @param similarityFunction 
      * @returns 
      */
-    private calculateWeights(similarityFunction: SentenceSimilarity): number[][] {
-        const result:number[][] = [];
+    private calculateWeights(similarityFunction: SentenceSimilarity): number[][] {        
         const sentences = this.text.sentences;
         const total = sentences.length;
+        const result:number[][] = []
+
         for(let i = 0; i < total; i++)
         {
-            result[i] = []
+            result[i] = Array<number>(total).fill(0)
             for(let j=0;j<total;j++)
             {
                 if(i === j)
                 {
+                    //diagonal
                     result[i][j] = 1;
+                }
+                else if(i > j)
+                {
+                    //matrix is symmetric
+                    result[i][j] = result[j][i]
                 }
                 else{
                     result[i][j] = similarityFunction.getSimilarity(sentences[i], sentences[j]);
