e1f17f56cfeb6c672427f56d84b3662340ab2c01
Nacho Brito <nacho.brito@nutritienda.com>
2022-02-26 08:58:02 +0100

    config: publication config

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-//import * as ConsoleLogger from "./TextRank/infraestructure/ConsoleLogger"
-//import * as LoggerInterface from "./TextRank/infraestructure/LoggerInterface"
-//import * as NullLogger from "./TextRank/infraestructure/NullLogger"
-import * as SummarizerConfig from "./TextRank/application/SummarizerConfig"
-import * as Summarizer from "./TextRank/application/Summarizer"
-import * as SentenceSimilarity from "./TextRank/domain/SentenceSimilarity"
-import * as Word from "./TextRank/domain/Word"
-import * as DefaultTextParser from "./TextRank/domain/DefaultTextParser"
-import * as Text from "./TextRank/domain/Text"
-import * as Sentence from "./TextRank/domain/Sentence"
-import * as SorensenDiceSimilarity from "./TextRank/domain/SorensenDiceSimilarity"
-import * as TextParser from "./TextRank/domain/TextParser"
-import * as Graph from "./TextRank/domain/Graph"
+import ConsoleLogger from "./TextRank/infraestructure/ConsoleLogger"
+import LoggerInterface from "./TextRank/infraestructure/LoggerInterface"
+import NullLogger from "./TextRank/infraestructure/NullLogger"
+import { AbsoluteSummarizerConfig,RelativeSummarizerConfig } from "./TextRank/application/SummarizerConfig"
+import Summarizer from "./TextRank/application/Summarizer"
+import SentenceSimilarity from "./TextRank/domain/SentenceSimilarity"
+import Word from "./TextRank/domain/Word"
+import DefaultTextParser from "./TextRank/domain/DefaultTextParser"
+import Text from "./TextRank/domain/Text"
+import Sentence from "./TextRank/domain/Sentence"
+import SorensenDiceSimilarity from "./TextRank/domain/SorensenDiceSimilarity"
+import TextParser from "./TextRank/domain/TextParser"
+import Graph from "./TextRank/domain/Graph"
 
 export {
-    SummarizerConfig,
+    ConsoleLogger,
+    LoggerInterface,
+    NullLogger,
+    AbsoluteSummarizerConfig,
+    RelativeSummarizerConfig,
     Summarizer,
     SentenceSimilarity,
     Word,
